b576f900bc6b347b4c916e4c37dd9cacf57a3c97
Martin Thoresen <martinhath@gmail.com>
2015-02-17 23:42:03 +0100

    Moved the docs from .c to .h

--- src/libcrypto.c
+++ src/libcrypto.c
@@ -53,15 +53,6 @@ static int rate_string(byte *str, size_t n)
 }
 
 
-/**
- * Breaks single character xor encoding of the
- * supplied string. Brute force, trying every
- * of the 256 chars.
- *
- * @param string Encoded string
- * @param n Length of the string
- * @return Most likely key
- */
 byte break_singlechar_xor(const byte *string, size_t n)
 {
     byte key, chr, *xor_bytes;
@@ -83,17 +74,6 @@ byte break_singlechar_xor(const byte *string, size_t n)
 }
 
 
-/**
- * Decrypts a repeated XOR string with the supplied key.
- * The encrypted string will be destroyed, and replaced
- * with the resulting string.
- * Both strings are assumed to be null-terminated.
- *
- * @param string Encrypted key.
- * @param key Key
- * @param n Length of string
- * @return 0 on success, any other value on failure.
- */
 int decrypt_repeat_xor(byte *string, byte *key, size_t n)
 {
     unsigned int i;
@@ -109,11 +89,6 @@ int decrypt_repeat_xor(byte *string, byte *key, size_t n)
     return 0;
 }
 
-/**
- * Converts a single char to a byte.
- * @param c Char to be converted
- * @return Converted char
- */
 byte char_to_byte(byte c)
 {
     if ('A' <= c && c <= 'F')
@@ -125,35 +100,12 @@ byte char_to_byte(byte c)
     return 0;
 }
 
-/**
- * Converts a single byte in string form to a char byte.
- * The function assumes that the string is well formed.
- *
- * Example: the string "20" would return 32.
- *
- * @param string The string containing one byte.
- * @return The byte.
- */
 byte str_to_byte(const char *string)
 {
     return char_to_byte((unsigned const char) *string) * 0x10 +
            char_to_byte((unsigned const char) *(string + 1));
 }
 
-/**
- * Convert a string to an array of the bytes which are
- * making up the string.
- *
- * For instance, the string "a068" should insert the
- * array {160, 104} in target.
- *
- * @param string The string to read from
- * @param target The target array.
- * @param n The length of string. This is assumed to be large enough
- * (that is, n >= (size(string)+1)/2)
- * @return The number of bytes written (since you could
- * potentially write '\0', thus terminating the string).
- */
 size_t str_to_bytes(const char *string, byte *target, size_t n)
 {
     size_t i;
@@ -172,12 +124,6 @@ size_t str_to_bytes(const char *string, byte *target, size_t n)
     return i / 2;
 }
 
-/**
- * 6 bit dec to base64 value
- *
- * @param n Decimal to be converted
- * @return Ascii value of base64 representation
- */
 char numtobase64(char n)
 {
     if (n <= 25)
@@ -189,12 +135,6 @@ char numtobase64(char n)
     return n == 62 ? '+' : '/';
 }
 
-/**
- * Base64 char to 6 bit dec.
- *
- * @param c Base64 char to be converted
- * @return 6 bit number.
- */
 char base64tonum(char c)
 {
     if (c == '+') return 62;
@@ -207,18 +147,6 @@ char base64tonum(char c)
     return -1;
 }
 
-/**
- * Byte array conversion to base64 byte array
- * The array size needs to be divisible by three.
- * In case the array length isn't divisible by three,
- * 0's should be appended.
- *
- * @param array The byte array
- * @param n Length of the array
- * @param b64 Array to store the base64 array. Is assumed to
- * be of size >= ceil(len/3)*4.
- * @return Pointer to the base64 array
- */
 char *byteatobase64a(byte *array, size_t n, char *b64)
 {
     size_t i, j;
@@ -235,15 +163,6 @@ char *byteatobase64a(byte *array, size_t n, char *b64)
     return b64;
 }
 
-/**
- * base64 string to byte string.
- *
- * @param b64string Base64 string to be converted
- * @param n Size of the Base64 string
- * @param array Target array to store the byte string in.
- * Size is assumed to be >= 3*strlen(b64)/4
- * @return Returns length of decoded string
- */
 size_t base64tobstring(const char *b64string, size_t n, byte *array)
 {
     size_t i, j, index;
@@ -268,14 +187,6 @@ size_t base64tobstring(const char *b64string, size_t n, byte *array)
     return len;
 }
 
-/**
- * XORs together two arrays, assumed to be length n
- *
- * @param a1 First array
- * @param a2 Second array
- * @param target Array to store the XOR in
- * @return n Pointer to target array
- */
 byte  *xor_array(byte *a1, byte *a2,
                           byte *target, size_t n)
 {
@@ -285,15 +196,6 @@ byte  *xor_array(byte *a1, byte *a2,
     return target;
 }
 
-/**
- * Computes the hamming distance of the two strings
- * s1 and s2 of length n.
- *
- * @param s1 First string
- * @param s2 Second string
- * @param n Length of both strings
- * @return Hamming distance
- */
 int hamming(byte *s1, byte *s2, size_t n)
 {
     size_t i;
@@ -304,13 +206,6 @@ int hamming(byte *s1, byte *s2, size_t n)
     return hamming;
 }
 
-/**
- * Computes the hamming distance of the two chars
- *
- * @param c1 First char
- * @param c2 Second char
- * @return Hamming distance
- */
 int hamming_char(byte c1, byte c2)
 {
     static byte BITS[8] = {1, 2, 4, 8, 16, 32, 64, 128};
--- src/libcrypto.h
+++ src/libcrypto.h
@@ -5,22 +5,127 @@
 
 typedef unsigned char byte;
 
-unsigned char char_to_byte(unsigned char);
-unsigned char str_to_byte(const char*);
-size_t str_to_bytes(const char*, unsigned char*, size_t);
+/**
+ * Converts a single char to a byte.
+ * @param c Char to be converted
+ * @return Converted char
+ */
+byte char_to_byte(byte c);
+/**
+ * Converts a single byte in string form to a char byte.
+ * The function assumes that the string is well formed.
+ *
+ * Example: the string "20" would return 32.
+ *
+ * @param string The string containing one byte.
+ * @return The byte.
+ */
+byte str_to_byte(const char *string);
+/**
+ * Convert a string to an array of the bytes which are
+ * making up the string.
+ *
+ * For instance, the string "a068" should insert the
+ * array {160, 104} in target.
+ *
+ * @param string The string to read from
+ * @param target The target array.
+ * @param n The length of string. This is assumed to be large enough
+ * (that is, n >= (size(string)+1)/2)
+ * @return The number of bytes written (since you could
+ * potentially write '\0', thus terminating the string).
+ */
+size_t str_to_bytes(const char *string, byte *target, size_t n);
 
-char numtobase64(char);
-char base64tonum(char);
+/**
+ * 6 bit dec to base64 value
+ *
+ * @param n Decimal to be converted
+ * @return Ascii value of base64 representation
+ */
+char numtobase64(char n);
+/**
+ * Base64 char to 6 bit dec.
+ *
+ * @param c Base64 char to be converted
+ * @return 6 bit number.
+ */
+char base64tonum(char c);
 
-char* byteatobase64a(unsigned char*, size_t, char*);
-size_t base64tobstring(const char*, size_t, unsigned char*);
+/**
+ * Byte array conversion to base64 byte array
+ * The array size needs to be divisible by three.
+ * In case the array length isn't divisible by three,
+ * 0's should be appended.
+ *
+ * @param array The byte array
+ * @param n Length of the array
+ * @param b64 Array to store the base64 array. Is assumed to
+ * be of size >= ceil(len/3)*4.
+ * @return Pointer to the base64 array
+ */
+char* byteatobase64a(byte *array, size_t n, char* b64);
+/**
+ * base64 string to byte string.
+ *
+ * @param b64string Base64 string to be converted
+ * @param n Size of the Base64 string
+ * @param array Target array to store the byte string in.
+ * Size is assumed to be >= 3*strlen(b64)/4
+ * @return Returns length of decoded string
+ */
+size_t base64tobstring(const char *b64string, size_t n, byte *array);
 
-unsigned char* xor_array(unsigned char*, unsigned char*, unsigned char*, size_t);
+/**
+ * XORs together two arrays, assumed to be length n
+ *
+ * @param a1 First array
+ * @param a2 Second array
+ * @param target Array to store the XOR in
+ * @return n Pointer to target array
+ */
+byte* xor_array(byte *a1, byte *a2, byte *target, size_t n);
 
-unsigned char break_singlechar_xor(const unsigned char*, size_t);
-int decrypt_repeat_xor(unsigned char*, unsigned char*, size_t n);
+/**
+ * Breaks single character xor encoding of the
+ * supplied string. Brute force, trying every
+ * of the 256 chars.
+ *
+ * @param string Encoded string
+ * @param n Length of the string
+ * @return Most likely key
+ */
+byte break_singlechar_xor(const byte *string, size_t n);
+/**
+ * Decrypts a repeated XOR string with the supplied key.
+ * The encrypted string will be destroyed, and replaced
+ * with the resulting string.
+ * Both strings are assumed to be null-terminated.
+ *
+ * @param string Encrypted key.
+ * @param key Key
+ * @param n Length of string
+ * @return 0 on success, any other value on failure.
+ */
+int decrypt_repeat_xor(byte *string, byte *key, size_t n);
 
-int hamming(unsigned char*, unsigned char*, size_t);
-int hamming_char(unsigned char, unsigned char);
+/**
+ * Computes the hamming distance of the two strings
+ * s1 and s2 of length n.
+ *
+ * @param s1 First string
+ * @param s2 Second string
+ * @param n Length of both strings
+ * @return Hamming distance
+ */
+int hamming(byte *s1, byte *s2, size_t n);
+/**
+ * Computes the hamming distance of the two chars
+ *
+ * @param c1 First char
+ * @param c2 Second char
+ * @return Hamming distance
+ */
+int hamming_char(byte c1, byte c2);
 
 #endif
